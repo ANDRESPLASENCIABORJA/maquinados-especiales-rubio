1fec3401c1c380143bcd027c2581dfbfdbfd5c68
APB98 <borja.basolutions@gmail.com>
2021-10-28 19:35:29 -0500

    creation of routes on App.js file

--- maquinados-especiales-rubio/src/App.js
+++ maquinados-especiales-rubio/src/App.js
@@ -5,17 +5,59 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Import materialize
-import 'materialize-css';
-
+import "materialize-css";
 
 // Import css stylesheet
-import './App.css';
+import "./App.css";
+
+// Import components to build the routes
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Import the subpages to test our routes
+import Home from "./pages/Home";
+import Certifications from "./pages/Certifications";
+import Contact from "./pages/Contact";
+import Philosophy from "./pages/Philosophy";
+import Politics from "./pages/Politics";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <Router>
+      <div className="application-header">
+        <Header />
+        <div className="main-sections">
+          <Switch>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/filosofia">
+              <Philosophy />
+            </Route>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/servicios">
+              <Services />
+            </Route>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/politica">
+              <Politics />
+            </Route>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/certificaciones">
+              <Certifications />
+            </Route>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/contacto">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
