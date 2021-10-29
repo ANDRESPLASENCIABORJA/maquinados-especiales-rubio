e54e36c5ac5b3302eee51a47bfcddf47045cdba5
APB98 <borja.basolutions@gmail.com>
2021-10-28 19:12:48 -0500

    creation of components header and footer

---
+++ maquinados-especiales-rubio/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// Import react
+import React from "react";
+
+// Create and export the footer function
+export default function Footer() {
+    return (
+        <header>
+            <h1>Hello footer</h1>
+        </header>
+    );
+}
---
+++ maquinados-especiales-rubio/src/components/Header.jsx
@@ -0,0 +1,11 @@
+// Import react
+import React from "react";
+
+// Create and export the header function
+export default function Header() {
+    return (
+        <header>
+            <h1>Hello header</h1>
+        </header>
+    );
+}
